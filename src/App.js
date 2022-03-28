cdd430cf2f14251d0901a7722f1eae83c72111fb
Shreya <hopeworld@hopeworld.local>
2022-03-27 19:34:40 -0700

    Add dashboard

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // import BookList from './components/BookList';
 import Header from './components/Header';
 import Search from './components/Search';
+import BooksApp from './components/Main';
 
 function App() {
   return (
     <div className="h-screen w-full font-mono">
+      {/* <BooksApp /> */}
       <Header />
       <Search />
       {/* <BookList /> */}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,15 +4,15 @@ const Header = () => {
   const [isToggle, setIsToggle] = useState(false);
 
   useEffect(() => {
-    setIsToggle(true);
-  },[])
+    isToggle(true);
+  })
 
   return (
     <div className="bg-sky-200 w-full"> {/* Navbar */}
       <div className="flex flex-row justify-evenly p-4 text-lg"> {/* Container */}
         <div className="flex flex-start" href="#home"><ion-icon name="book-outline"></ion-icon></div> {/* Logo */}
         <div className="" /> {/* Menu Toggle */}
-        <div className="w-full flex flex-end">  {/* Menu Button */}
+        <div onChange={() => setIsToggle(true)} className="w-full flex flex-end">  {/* Menu Button */}
           <ion-icon name="apps-outline"></ion-icon>
           <div className="flex">  {/* Login */}
             Signed in as: <a href="#login">User Signed In</a>
